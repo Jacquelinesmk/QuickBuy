dcd6a774eae79721b0dac3de708807faed08d80e
Jacqueline Sayuri Michelucci Kiko e <jsayuri@fitec.org.br>
2019-05-07 13:56:29 -0300

    Implementando validação nas classes - Regra de Negócios

--- QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -11,22 +11,22 @@ namespace QuickBuy.Dominio.ObjetoDeValor
 		public string Nome { get; set; }
 		public string Descricao { get; set; }
 
-		public bool isBoleto
+		public bool IsBoleto
 		{
 			get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
 		}
 
-		public bool isCartaoCredito
+		public bool IsCartaoCredito
 		{
 			get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
 		}
 
-		public bool isDeposito
+		public bool IsDeposito
 		{
 			get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
 		}
 
-		public bool isNaoDefinido
+		public bool IsNaoDefinido
 		{
 			get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
 		}
