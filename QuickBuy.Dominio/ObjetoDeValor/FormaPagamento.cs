4e413a2098d1caf4d12bca04a732f9fd9d183ecf
Jacqueline Sayuri Michelucci Kiko e <jsayuri@fitec.org.br>
2019-05-07 13:28:17 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,34 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+	public class FormaPagamento
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string Descricao { get; set; }
+
+		public bool isBoleto
+		{
+			get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+		}
+
+		public bool isCartaoCredito
+		{
+			get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+		}
+
+		public bool isDeposito
+		{
+			get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+		}
+
+		public bool isNaoDefinido
+		{
+			get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+		}
+	}
+}
