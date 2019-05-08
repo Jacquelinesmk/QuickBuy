a329a10a3dae114e1bf41d1097ab91cd60113d24
Jacqueline Sayuri Michelucci Kiko e <jsayuri@fitec.org.br>
2019-05-08 10:48:17 -0300

    Configurando conexão com o banco de dados mysql

--- QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
 using QuickBuy.Dominio.Entidades;
 using QuickBuy.Dominio.ObjetoDeValor;
 using System;
@@ -9,10 +10,16 @@ namespace QuickBuy.Repositorio.Contexto
 {
 	public class QuickBuyContexto : DbContext
 	{
+
 		public DbSet<Usuario> Usuarios { get; set; }
 		public DbSet<Produto> Produtos { get; set; }
 		public DbSet<Pedido> Pedidos { get; set; }
 		public DbSet<ItemPedido> ItensPedidos { get; set; }
 		public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+		public QuickBuyContexto(DbContextOptions options) : base(options)
+		{
+
+		}
 	}
 }
