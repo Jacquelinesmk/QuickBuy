7b4a778272e646350ffb23c431ec6e7286713b71
Jacqueline Sayuri Michelucci Kiko e <jsayuri@fitec.org.br>
2019-05-08 11:33:57 -0300

    Mapeando as propriedades

--- QuickBuy.Repositorio/Config/UsuarioConfiguration.cs
+++ QuickBuy.Repositorio/Config/UsuarioConfiguration.cs
@@ -4,11 +4,35 @@ using QuickBuy.Dominio.Entidades;
 
 namespace QuickBuy.Repositorio.Config
 {
+	//Configurando a tabela Usuario do banco de dados
 	public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
 	{
 		public void Configure(EntityTypeBuilder<Usuario> builder)
 		{
-			throw new System.NotImplementedException();
+			builder.HasKey(u => u.Id);
+
+			//Builder utiliza o padrão Fluent
+			builder
+				.Property(u => u.Email)
+				.IsRequired()
+				.HasMaxLength(50);
+
+			builder
+				.Property(u => u.Senha)
+				.IsRequired()
+				.HasMaxLength(400);
+
+			builder
+				.Property(u => u.Nome)
+				.IsRequired()
+				.HasMaxLength(50);
+
+			builder
+				.Property(u => u.Sobrenome)
+				.IsRequired()
+				.HasMaxLength(50);
+
+			//builder.Property(u => u.Pedidos);
 		}
 	}
 }
